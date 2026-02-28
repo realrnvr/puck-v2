0c7cdff90c0ccf2f36aa98b5989193699f37aa8d
realrnvr <realrnvr@gmail.com>
2026-03-01 00:19:52 +0530

    formatt

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,12 @@
+import "./resets.css";
+import "./utils.css";
+import "./index.css";
+
 import RouterProviderBase from "./providers/RouterProviderBase";
 import QueryClientProviderBase from "./providers/QueryClientProviderBase";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import "./resets.css";
-import "./utils.css";
-import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
