29c3ff68e9855f7711c5d1cd1168f656ab618765
realrnvr <realrnvr@gmail.com>
2026-03-18 00:42:54 +0530

    router addition : [GET "/chapter-image/:chapterId" + image proxy]

--- server/src/router/manga.router.js
+++ server/src/router/manga.router.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import {
+  getChapterImages,
+  getChapterStatics,
   getChapterVariants,
   getMangaAggregate,
   getMangaChapters,
+  proxyChapterImage,
 } from "../controller/manga.controller.js";
 
 export const router = Router();
@@ -10,3 +13,9 @@ export const router = Router();
 router.route("/:mangaId/chapters").get(getMangaChapters);
 router.route("/:mangaId/aggregate").get(getMangaAggregate);
 router.route("/:mangaId/chapter").get(getChapterVariants);
+router.route("/chapter/:chapterId").get(getChapterStatics);
+router.route("/chapter-image/:chapterId").get(getChapterImages);
+// ${process.env.SERVER_BASE_URL}/api/v1/manga/proxy/chapter-image/${encodeURIComponent(baseUrl)}/${quality}/${hash}/${val}
+router
+  .route("/proxy/chapter-image/:baseUrl/:quality/:hash/:val")
+  .get(proxyChapterImage);
