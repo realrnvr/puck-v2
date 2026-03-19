e24a09a9f0cfc3aa0813f95c24d51c9651713ca3
realrnvr <realrnvr@gmail.com>
2026-03-20 00:48:40 +0530

    window provider addition : [whole app can now use window modal]

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,12 +8,15 @@ import QueryClientProviderBase from "./providers/QueryClientProviderBase";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { WindowProvider } from "./features/window_system/provider/WindowProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProviderBase>
-      <RouterProviderBase />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <WindowProvider>
+        <RouterProviderBase />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </WindowProvider>
     </QueryClientProviderBase>
   </StrictMode>,
 );
