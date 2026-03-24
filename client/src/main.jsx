8f2a8de15efab2ce1e2f7ca051395b96f2f597a4
realrnvr <realrnvr@gmail.com>
2026-03-25 04:32:26 +0530

    socket added + folder structure changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,21 +2,24 @@ import "./resets.css";
 import "./utils.css";
 import "./index.css";
 
-import RouterProviderBase from "./providers/RouterProviderBase";
-import QueryClientProviderBase from "./providers/QueryClientProviderBase";
+import RouterProviderBase from "./providers/react_router/RouterProviderBase";
+import QueryClientProviderBase from "./providers/tanstack_query/QueryClientProviderBase";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { WindowProvider } from "./features/window_system/provider/WindowProvider";
+import { SocketProvider } from "./core/socket/provider/SocketProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <QueryClientProviderBase>
-      <WindowProvider>
-        <RouterProviderBase />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </WindowProvider>
-    </QueryClientProviderBase>
+    <SocketProvider>
+      <QueryClientProviderBase>
+        <WindowProvider>
+          <RouterProviderBase />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </WindowProvider>
+      </QueryClientProviderBase>
+    </SocketProvider>
   </StrictMode>,
 );
