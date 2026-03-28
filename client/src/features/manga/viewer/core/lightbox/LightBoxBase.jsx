1c456a6135c5fd1d5c2c6c8e0bae014ff5017b9c
realrnvr <realrnvr@gmail.com>
2026-03-28 17:57:51 +0530

    comp refactor

--- client/src/features/manga/viewer/core/lightbox/LightBoxBase.jsx
+++ client/src/features/manga/viewer/core/lightbox/LightBoxBase.jsx
@@ -2,6 +2,7 @@ import "yet-another-react-lightbox/plugins/counter.css";
 import "yet-another-react-lightbox/styles.css";
 import "./light-box-base.css";
 
+import React from "react";
 import Lightbox from "yet-another-react-lightbox";
 import Inline from "yet-another-react-lightbox/plugins/inline";
 import Counter from "yet-another-react-lightbox/plugins/counter";
@@ -12,51 +13,38 @@ import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import ReaderPlugin from "../../plugin/ReaderPlugin";
 
-import { useState } from "react";
-import { animationOptions, imageFitOptions } from "../../assets/data";
 import { useChapterImages } from "../../hooks/useChapterImages";
+import { useLightBoxProps } from "../../hooks/useLightBoxProps";
+import { useMangaController } from "../../hooks/useMangaController";
 
-export default function LightBoxBase() {
+export default function LightBoxBase({ onEnterFullscreen, onExitFullscreen }) {
   const images = useChapterImages();
+  const { carousel, animation, zoom, isRTL } = useLightBoxProps();
+  const { navigation } = useMangaController();
 
-  // carousel
-  const [carousel, setCarousel] = useState({
-    finite: true,
-    preload: [2],
-    padding: [16],
-    spacing: [30],
-    imageFit: imageFitOptions[0],
-  });
+  React.useEffect(() => {
+    function handleKeyDown(e) {
+      if (
+        images.isIndexAtEnd &&
+        e.key === (isRTL ? "ArrowLeft" : "ArrowRight")
+      ) {
+        (isRTL ? navigation.prev : navigation.next)();
+      }
 
-  // animation
-  const [animation, setAnimation] = useState({
-    swipe: [500],
-    easingSwipe: animationOptions[2],
-    easingNavigation: animationOptions[3],
-    zoom: [500],
-  });
+      if (
+        images.isIndexAtStart &&
+        e.key === (isRTL ? "ArrowRight" : "ArrowLeft")
+      ) {
+        (isRTL ? navigation.next : navigation.prev)();
+      }
+    }
 
-  // zoom
-  const [zoom, setZoom] = useState({
-    minZoom: [1],
-    maxZoomPixelRatio: [1],
-    zoomInMultiplier: [2],
-    doubleTapDelay: [300],
-    doubleClickDelay: [500],
-    doubleClickMaxStops: [2],
-    keyboardMoveDistance: [50],
-    wheelZoomDistanceFactor: [100],
-    pinchZoomDistanceFactor: [100],
-  });
-
-  // slideshow
-  const [slideshow, setSlideshow] = useState({
-    autoplay: false,
-    delay: [3000],
-  });
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [navigation, images.isIndexAtEnd, images.isIndexAtStart, isRTL]);
 
   return (
-    <div className="light-box-base">
+    <>
       <Lightbox
         index={images.deferredIndex}
         slides={images.data}
@@ -70,8 +58,13 @@ export default function LightBoxBase() {
           Zoom,
           ReaderPlugin,
         ]}
-        // portal={{ container: { dir: "rtl" } }}
-        // inline={{ dir: "rtl" }}
+        inline={{
+          dir: isRTL ? "rtl" : "ltr",
+          style: {
+            height: "100%",
+            width: "100%",
+          },
+        }}
         carousel={{
           finite: carousel.finite,
           preload: carousel.preload[0],
@@ -100,27 +93,14 @@ export default function LightBoxBase() {
           wheelZoomDistanceFactor: zoom.wheelZoomDistanceFactor[0],
           pinchZoomDistanceFactor: zoom.pinchZoomDistanceFactor[0],
         }}
-        control={{
-          carousel,
-          setCarousel,
-          animation,
-          setAnimation,
-          zoom,
-          setZoom,
-          slideshow,
-          setSlideshow,
-          pageIndex: images.index,
-          setPageIndex: images.setIndex,
-          slidesLength: images.data.length - 1,
-          isAtStart: images.isIndexAtStart,
-          isAtEnd: images.isIndexAtEnd,
-        }}
         on={{
           view: ({ index: currentIndex }) => {
             images.handleIndexChange(currentIndex);
           },
+          enterFullscreen: () => onEnterFullscreen(),
+          exitFullscreen: () => onExitFullscreen(),
         }}
       />
-    </div>
+    </>
   );
 }
